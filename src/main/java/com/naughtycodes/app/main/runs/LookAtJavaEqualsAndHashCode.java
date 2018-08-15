3fdd5488bea094e9adfffaa52f8294d1ab7e9646
= <=>
2018-08-16 00:28:51 +0530

    have to test equals and hashcode methods

--- src/main/java/com/naughtycodes/app/main/runs/LookAtJavaEqualsAndHashCode.java
+++ src/main/java/com/naughtycodes/app/main/runs/LookAtJavaEqualsAndHashCode.java
@@ -1,10 +1,35 @@
 package com.naughtycodes.app.main.runs;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 public class LookAtJavaEqualsAndHashCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		List<Person> p = new ArrayList<Person>();
+		LookAtJavaEqualsAndHashCode run = new LookAtJavaEqualsAndHashCode();
+		run.loadData(p);
 
+	}
+	
+	public void loadData(List<Person> p) {
+		
+		for(int i = 0; i <= 1; i++) {
+			p.add(new Person(i, "Name_"+i, "Some description_"+i, i));
+		}
+		
+		p.add(new Person(0, "Name_"+0, "Some description_"+0, 0));
+		
+		for(Person t : p)
+			System.out.println(t.toString());
+		
+		
 	}
 
 }
@@ -15,6 +40,47 @@ class Person {
 	private String name;
 	private String description;
 	private int age;
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if(obj == this) {
+			return true;
+		}
+		
+		if(obj == null || obj.getClass() != this.getClass()) {
+			return false;
+		}
+		
+		Person tObj = (Person) obj;
+
+//		return id == tObj.id 
+//				&& ( name == tObj.name || (name != null && name.equals(tObj.getName()) ))
+//				&& ( description == tObj.description || (description != null && description.equals(tObj.getDescription())) )
+//				&& ( age == tObj.age);
+		
+		System.out.println(this.id+" "+this.name+" "+this.description+" "+this.age);
+		System.out.println(tObj.toString());
+		
+	    return new EqualsBuilder()
+	    		.append(id, tObj.id)
+	    	    .append(name, tObj.name)
+	    	    .append(description, tObj.description)
+	    	    .append(age, tObj.age)
+	    	    .isEquals();
+
+	}
+	
+	@Override
+	public int hashCode()
+	   {
+	      return new HashCodeBuilder()
+	         .append(id)
+	         .append(name)
+	         .append(description)
+	         .append(age)
+	         .toHashCode();
+	   }
 	
 	public Person() {
 		super();
@@ -53,9 +119,11 @@ class Person {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", description=" + description + ", age=" + age + "]";
+	}
 	
 	
 }
